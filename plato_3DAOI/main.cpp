076a9cba0232ee8b1dfc4b2bf8dfe6bec91bafd3
plato <plato@mystery.com>
2017-11-22 11:27:30 +0800

    修改内容： 初步完成SDK命名空间下的CustomException类

--- plato_3DAOI/main.cpp
+++ plato_3DAOI/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+#include "SDK/customexception.hpp"
+
 using namespace std;
+
+void func()
+{
+    THROW_EXCEPTION("Argument invaild!");
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    try
+    {
+        func();
+    }
+
+    catch(SDK::CustomException& ex)
+    {
+        cout << ex.what() << endl;
+    }
+
     return 0;
 }
