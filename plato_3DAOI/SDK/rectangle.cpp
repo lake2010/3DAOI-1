5eeff033ef43c17e4c1b50c671b45494e65fbfd6
plato <plato@mystery.com>
2017-11-22 17:46:07 +0800

    修该内容： 初步完成了SDK命名空间下的Rectangle类

--- plato_3DAOI/SDK/rectangle.cpp
+++ plato_3DAOI/SDK/rectangle.cpp
@@ -4,5 +4,15 @@ using namespace SDK;
 
 Rectangle::Rectangle()
 {
+    // 成员变量初始化
+    this->m_posX = 0;
+    this->m_posY = 0;
+    this->m_width = 0;
+    this->m_height = 0;
+    this->m_angle = 0;
+}
+
+Rectangle::~Rectangle()
+{
 
 }
